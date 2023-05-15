c67e8b1c4780f29e444cdab5e7693ec70defa0fc
SaketJawa <132909470+SaketJawa@users.noreply.github.com>
2023-05-15 10:15:58 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ let b = 5;
 let c;
 
 // alert( "description of alerted_value: " + alerted_value );
-let output = 
+let print = 
     `
     let a = 3;
     let b = 5;
@@ -28,7 +28,7 @@ let output =
     !a || !c  = ${!a || !c }
     `;
 
-alert(output);
+alert(print);
 /*
 a + b
 a - b
@@ -47,3 +47,12 @@ a < b
 !a && !c 
 !a || !c 
 */
+
+
+let first_name = "Saket";
+let last_name = "Jawaji";
+let email = "jawa0016@algonquinlive.com"
+
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}` ;
+
+alert(output);
